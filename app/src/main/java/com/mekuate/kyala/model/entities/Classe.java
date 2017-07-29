6dfe4d118477a17f3b8f52f5466cf642e1c5c180
mekuate <giselemekuate@gmail.com>
2017-07-29 13:06:10 +0100

    first commit

---
+++ app/src/main/java/com/mekuate/kyala/model/entities/Classe.java
@@ -0,0 +1,85 @@
+package com.mekuate.kyala.model.entities;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.util.HashMap;
+
+/**
+ * Created by Mekuate on 20/05/2017.
+ */
+
+public class Classe  implements Parcelable {
+    private String id;
+    private String nom;
+   private HashMap<String,Boolean> niveau;
+    private HashMap <String,Boolean> matiere;
+
+    public Classe() {
+    }
+
+    public Classe( String id,String nom) {
+        this.nom = nom;
+        this.id = id;
+    }
+
+    protected Classe(Parcel in) {
+        id = in.readString();
+        nom = in.readString();
+    }
+
+    public static final Creator<Classe> CREATOR = new Creator<Classe>() {
+        @Override
+        public Classe createFromParcel(Parcel in) {
+            return new Classe(in);
+        }
+
+        @Override
+        public Classe[] newArray(int size) {
+            return new Classe[size];
+        }
+    };
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public HashMap<String, Boolean> getMatiere() {
+        return matiere;
+    }
+
+    public void setMatiere(HashMap<String, Boolean> matiere) {
+        this.matiere = matiere;
+    }
+
+    public HashMap<String, Boolean> getNiveau() {
+        return niveau;
+    }
+
+    public void setNiveau(HashMap<String, Boolean> niveau) {
+        this.niveau = niveau;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(nom);
+    }
+}
